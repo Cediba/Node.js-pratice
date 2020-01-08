417080cb321dfff5b724f1c8880dfc3ab8abab5e
Cedric <cedric.balmer@gmx.ch>
2020-01-08 15:45:47 -0800

    doing jun.8 practice

--- jan.8/routes/main-router.js
+++ jan.8/routes/main-router.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../view', 'home.html'));
+    res.sendFile(path.join(__dirname, '../views', 'home.html'));
 });
 
+router.get('/note-page', (req, res, next)=> {
+    res.sendFile(path.join(__dirname, '../views', 'form.html'));
+});
 
+router.post('/send-message', (req, res, next) => {
+    fs.writeFile('note.txt', req.body.note, () => {
+        res.redirect('/');
+    });
 
+router.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../views', '404.html'));
+});
+
+});
 
 module.exports = router;
