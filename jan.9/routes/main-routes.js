ce3ab310d7e89baebdf5cbfb53e16a140f8bd2d5
Cedric <cedric.balmer@gmx.ch>
2020-01-09 14:24:19 -0800

    updated inClass code

--- jan.9/routes/main-routes.js
+++ jan.9/routes/main-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+const fs = require('fs');
 
 const data = [];
 
@@ -14,7 +15,7 @@ router.post('/note', (req, res, next) => {
     fs.writeFile('notes.txt', JSON.stringify(data), () => {
         res.status(302);
         res.redirect('/');
-    })
+    });
 
 })
 
